14259e81d9705b28365e25e79c1b715e2c5089cc
Dipsan Dhital <dipsandhital12@gmail.com>
2025-12-23 17:02:27 +0545

    Creating the Layout and Context

--- cloudex/src/App.tsx
+++ cloudex/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+import Layout from "./components/Layout"
+import { ThemeProvider } from "./components/ui/context/theme-provider"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <ThemeProvider>
+      <Layout/>
+    </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
---
+++ cloudex/src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import type { PropsWithChildren } from "react"
+import Header from "./Header"
+
+const Layout = ({children}:PropsWithChildren) => {
+  return (
+    <div className="bg-gradient-to-br from-background to-muted ">
+        {/*  Header */}
+            <Header/>
+        <main className="min-h-screen container mx-auto px-4 py-8 ">
+            {children}
+        </main>
+
+        {/*  Footer  */}
+        <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter:bg-background/60] ">
+            <div className="container mx-auto px-4 text-center text-gray-400 ">
+                <p> Masde with 🔥 by Dipsan Dhital </p>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Layout
